bbd8084ca954a5a785b16df779c2abe95c56933c
arikarim <arikarim146@gmail.com>
2021-04-24 01:19:10 +0300

    Insert foods to the food page

--- src/food.js
+++ src/food.js
@@ -1,4 +1,7 @@
 import Kabab from './img/kababb.jpg'
+import Kabab from './img/kababb.jpg'
+import Kabab from './img/kababb.jpg'
+import Kabab from './img/kababb.jpg'
 
 const foodpage = (() => {
   const contentContainer = document.querySelector("#content");
@@ -6,11 +9,23 @@ const foodpage = (() => {
   food.classList.add('container-fluid')
   food.setAttribute("id", "menu");
   food.setAttribute("data-tab-content", "");
+
+
   food.innerHTML = `
   <div class='row'>
     <div class='col-12 col-md-6 d-flex'>
-      <div class='col-6 image-food'>
-        <img class='img-fluid' src="./img/kuba.jpg" alt="" />
+      <div id='img1' class='col-6 image-food  img1'>
+        
+      </div>
+      <div class='col-6'>
+        <h3 class='text-center'>KABAB</h3>
+        <p>Kebabs are various cooked meat dishes with their origins in Middle Eastern cuisine. Numerous variants are popular around the world. In many parts of Asia, the Muslim world, and in Indian English and the languages of the Middle East, a kebab is any of a wide variety of grilled meat dishes. </p>
+      </div>
+    </div>
+
+    <div class='col-12 col-md-6 d-flex'>
+      <div id='img1' class='col-6 image-food  img1'>
+        
       </div>
       <div class='col-6'>
         <h3 class='text-center'>KABAB</h3>
@@ -19,5 +34,10 @@ const foodpage = (() => {
     </div>
   </div>`;
   contentContainer.appendChild(food)
+  const img = new Image();
+  img.src = Kabab;
+  img.classList.add('image-food')
+  const img2 = document.getElementById('img1');
+ img2.appendChild(img)
 })();
 export {foodpage};
