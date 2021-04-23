1a73648b2def55a7b248836eb1179bc2aa25481e
arikarim <arikarim146@gmail.com>
2021-04-23 22:55:02 +0300

    Add header links

--- src/home.js
+++ src/home.js
@@ -3,13 +3,16 @@ const renderHome = (() =>{
   const container = document.getElementById('content');
 
   const home = document.createElement('div');
+  home.setAttribute("data-tab-content", "");
 
-  home.classList.add('home-content');
+  home.classList.add('tab-content');
   home.innerHTML = `
-  <div class='container position-relative'>
-    <div class='row col-6 mt-5 mx-auto main-row py-5'>
-      <h1 class='py-5 text-center'>Kurdistan Resturant</h1>
-      <h4 class='text-center'>Come and tase some delicious local foods</h4>
+  <div id="home" class="active" data-tab-content>
+    <div class='container position-relative'>
+      <div class='row col-6 mt-5 mx-auto main-row py-5'>
+        <h1 class='py-5 text-center'>Kurdistan Resturant</h1>
+        <h4 class='text-center'>Come and tase some delicious local foods</h4>
+      </div>
     </div>
   </div>`;
 
